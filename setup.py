cbf46b1b797cd799b083b78573032965976b1077
DIPENDRA KUMAR SHAH <74634983+invincibleRD@users.noreply.github.com>
2024-08-23 18:37:18 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="gpt-4o-cli",
-    version="0.1.1",
+    version="0.1.4",
     packages=find_packages(), 
     install_requires=[
         "openai",
